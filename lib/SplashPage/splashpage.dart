a563511ee7dccce8b6667ca710fbe4c48e005fb3
Pranjal <pranjalbharat07@gmail.com>
2024-03-16 11:17:11 +0530

    splashscreen added

---
+++ lib/SplashPage/splashpage.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:tunedheart/Login/login_page.dart';
+import 'package:tunedheart/Rooms/room_page.dart';
+import 'package:tunedheart/Services/FireAuth%20Service/authentication.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  Future<void> _checkAuthenticationAndNavigate() async {
+    if (Authenticate.isLoggedIn()) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const RoomPage()),
+      );
+    } else {
+    
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginPage()),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Use WidgetsBinding to schedule navigation after build.
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(seconds: 5), () {
+        _checkAuthenticationAndNavigate();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+      return const Scaffold(
+ body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlutterLogo(size: 100),
+            SizedBox(height: 20),
+            Text(
+              'Welcome to TunedHeartz',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tunedheart/Login/login_page.dart';
+import 'package:tunedheart/SplashPage/splashpage.dart';
 import 'package:tunedheart/firebase_options.dart';
 
 Future main() async {
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(),
+      home: const SplashPage(),
     );
   }
 }
